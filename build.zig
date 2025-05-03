b3c126c7fee4992f3cc8b955c24bfbe5dd444aa7
NSteczynski <norbertsteczynski@gmail.com>
2025-05-03 09:14:39 +0200

    initial commit

---
+++ build.zig
@@ -0,0 +1,39 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("src/mefix.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "mefix",
+        .root_module = exe_mod,
+        .use_llvm = false,
+        .use_lld = false,
+    });
+
+    b.installArtifact(exe);
+
+    const glfw_dep = b.dependency("glfw", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.root_module.addImport("glfw", glfw_dep.module("glfw"));
+
+    const zgl = b.dependency("zgl", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.root_module.addImport("zgl", zgl.module("zgl"));
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
---
+++ src/mefix.zig
@@ -0,0 +1,5 @@
+const std = @import("std");
+const zgl = @import("zgl");
+const glfw = @import("glfw");
+
+pub fn main() void {}
