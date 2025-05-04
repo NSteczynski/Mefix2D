cbf00facaf921202a6ae157963c10e1586986a4d
NSteczynski <norbertsteczynski@gmail.com>
2025-05-04 09:33:10 +0200

    remove constant values from window initialization

--- src/mefix.zig
+++ src/mefix.zig
@@ -1,8 +1,8 @@
 const window = @import("core/window.zig");
 const gl = @import("core/gl.zig");
 
-pub fn init() void {
-    window.init(800, 600, "mefix2D");
+pub fn init(width: u32, height: u32, title: [*:0]const u8) void {
+    window.init(width, height, title);
     gl.init();
 }
 
