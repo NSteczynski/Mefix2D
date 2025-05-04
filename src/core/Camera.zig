755b8739c069aa8c1ee87b8f01c3c55aae25d1bc
NSteczynski <norbertsteczynski@gmail.com>
2025-05-04 16:44:15 +0200

    remove constant downscale in camera

--- src/core/Camera.zig
+++ src/core/Camera.zig
@@ -4,6 +4,7 @@ const renderer = @import("../graphics/renderer.zig");
 const Self = @This();
 
 position: [2]f32 = .{ 0, 0 },
+downscale: f32 = 1,
 
 pub const new = Self{};
 
@@ -11,8 +12,8 @@ pub fn render(self: Self) void {
     // Move the entities to opposite direction than camera and downscale the coords by tenth.
     // Last row contains all zeros instead of (0, 0, 1) to zero the z-index otherwise it will not render.
     renderer.setMatrixUniform("u_View", [3][3]f32{
-        .{ 0.1, 0, -self.position[0] },
-        .{ 0, 0.1, -self.position[1] },
+        .{ self.downscale, 0, -self.position[0] },
+        .{ 0, self.downscale, -self.position[1] },
         .{ 0, 0, 0 },
     });
 }
