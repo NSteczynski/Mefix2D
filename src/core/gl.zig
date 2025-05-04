fabe3d2050b1cd7a19934326c71f0aaec897a399
NSteczynski <norbertsteczynski@gmail.com>
2025-05-04 09:07:37 +0200

    render opengl context inside window

---
+++ src/core/gl.zig
@@ -0,0 +1,40 @@
+const std = @import("std");
+const gl = @import("zgl");
+const glfw = @import("glfw");
+const window = @import("window.zig");
+
+fn glGetProcAddress(_: glfw.GLProc, proc: [:0]const u8) ?gl.binding.FunctionPointer {
+    return glfw.getProcAddress(proc);
+}
+
+pub fn init() void {
+    const proc: glfw.GLProc = undefined;
+    gl.loadExtensions(proc, glGetProcAddress) catch |err| {
+        std.log.err("Failed to load OpenGL functions: {!}!", .{err});
+        return std.process.exit(0);
+    };
+
+    window.setRefresh(refresh);
+
+    gl.enable(.depth_test);
+    gl.depthFunc(.less);
+
+    gl.enable(.cull_face);
+    gl.cullFace(.back);
+    gl.frontFace(.ccw);
+
+    gl.enable(.blend);
+    gl.blendFunc(.src_alpha, .one_minus_src_alpha);
+
+    gl.polygonMode(.front_and_back, .fill);
+}
+
+pub fn clearBackground() void {
+    gl.clearColor(0.53, 0.81, 0.92, 1.0);
+    gl.clear(.{ .color = true, .depth = true });
+}
+
+fn refresh(glfw_window: glfw.Window) void {
+    const size = glfw_window.getSize();
+    gl.viewport(0, 0, size.width, size.height);
+}
--- src/mefix.zig
+++ src/mefix.zig
@@ -1,12 +1,18 @@
-const std = @import("std");
 const window = @import("core/window.zig");
+const gl = @import("core/gl.zig");
 
 pub fn main() void {
     window.init(800, 600, "mefix2D");
     defer window.deinit();
+    gl.init();
 
     while (!window.shouldClose()) {
         window.pollEvents();
         defer window.swapBuffers();
+
+        // Render
+        {
+            gl.clearBackground();
+        }
     }
 }
